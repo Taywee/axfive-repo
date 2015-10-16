1686b2771ba1b3e8436a2040d65cc7852c04e5e8
Taylor C. Richberger <Taywee@gmx.com>
2015-10-16 16:04:25 -0600

    buildall.sh: pulls repos as well

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+for repo in ~/repos/*; do
+    pushd "$repo"
+    git pull
+    echo cp "$repo"/pkg/rpm/*.spec ~/rpmbuild/SPECS/
+    cp "$repo"/pkg/rpm/*.spec ~/rpmbuild/SPECS/
+    popd
+done
+
 for spec in rpmbuild ~/rpmbuild/SPECS/*.spec; do
     spectool -g -f -C ~/rpmbuild/SOURCES "$spec"
     rpmbuild -bb "$spec"
 done
-
